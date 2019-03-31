c7488fb8aaebe88421b31ae4752cf81a902d9268
Christopher Fuentes <fuenteschris75@gmail.com>
2019-03-31 13:25:41 -0600

    Final Project

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyD7LpaJbUnhshRuts5y5-qaE-gsiM1AzWA",
+    authDomain: "notesapp-29a9d.firebaseapp.com",
+    databaseURL: "https://notesapp-29a9d.firebaseio.com",
+    projectId: "notesapp-29a9d",
+    storageBucket: "notesapp-29a9d.appspot.com",
+    messagingSenderId: "1082809867901"
+}
